3acd2a6e65a3f5624c42fe67d5209203fd9266a8
Sebastian Paarmann <git@s-paarmann.de>
2023-08-17 15:15:20 +0200

    Progress on Chapter 4

--- Chapter4.thy
+++ Chapter4.thy
@@ -30,4 +30,114 @@ lemma "ord t \<Longrightarrow> ord (ins x t)"
     apply (auto simp add: ins_inserts)
   done
 
+chapter \<open>Chapter 4.5\<close>
+
+inductive ev :: "nat \<Rightarrow> bool" where
+  ev0:  "ev 0"
+| evSS: "ev n \<Longrightarrow> ev (Suc (Suc n))"
+
+fun evn :: "nat \<Rightarrow> bool" where
+  "evn 0 = True"
+| "evn (Suc 0) = False"
+| "evn (Suc (Suc n)) = evn n"
+
+lemma "ev n \<Longrightarrow> evn n"
+  apply (induction rule: ev.induct)
+  by (simp_all)
+
+lemma "evn n \<Longrightarrow> ev n"
+  apply (induction n rule: evn.induct)
+  by (simp_all add: ev0 evSS)
+
+(* Just to show it can be done in one go; separating out the subgoals *)
+lemma "ev n = evn n"
+  apply (rule iffI)
+  subgoal apply (induction rule: ev.induct) by (simp_all)
+  apply (induction n rule: evn.induct) by (simp_all add: ev0 evSS)
+
+(* ev0 and evSS (i.e. inductive rules in general) are not automatically turned into
+   simp and intro rules, unlike the rules in the evn "fun" definition. This is because
+   "fun" definitions have a termination requirement, but inductive definitions do not.
+   In this case, they *do* make sense as such rules, so we can declare them as such: *)
+declare ev.intros[simp, intro]
+
+(* "for r" says that "r" is a "fixed" parameter of star, which lets Isabelle generate
+   simpler induction rules *)
+inductive star :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" for r where
+  refl:   "star r x x"
+| step:   "r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
+ 
+lemma star_trans: "star r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
+  apply (induction rule: star.induct)
+  apply (assumption)
+  apply (metis step)
+  done
+
+(* Exercises *)
+
+(* 4.2 *)
+inductive palindrome :: "'a list \<Rightarrow> bool" where
+  empty:      "palindrome []"
+| singleton:  "palindrome [x]"
+| wrap:       "palindrome xs \<Longrightarrow> palindrome (a # xs @ [a])"
+
+lemma "palindrome xs \<Longrightarrow> rev xs = xs"
+  apply (induction rule: palindrome.induct)
+  by (simp_all)
+
+(* 4.3 *)
+inductive star' :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" for r where
+  refl':  "star' r x x"
+| step':  "star' r x y \<Longrightarrow> r y z \<Longrightarrow> star' r x z"
+
+lemma star_inv_step: "star r x y \<Longrightarrow> r y z \<Longrightarrow> star r x z"
+  apply (induction rule: star.induct)
+  apply (rule step[where ?y = z])
+    apply (assumption)
+    apply (rule refl)
+  apply (metis step)
+  done
+
+lemma "star' r x y \<Longrightarrow> star r x y"
+  apply (induction rule: star'.induct)
+  apply (rule refl)
+  apply (simp add: star_inv_step)
+  done
+
+lemma star'_inv_step: "r x y \<Longrightarrow> star' r y z \<Longrightarrow> star' r x z"
+  apply (rotate_tac) (* induction happens on first assumption, rotate modifies order *)
+  apply (induction rule: star'.induct)
+  apply (rule step'[where ?y = x])
+    apply (rule refl')
+    apply (assumption)
+  apply (metis step')
+  done
+
+lemma "star r x y \<Longrightarrow> star' r x y"
+  apply (induction rule: star.induct)
+  apply (rule refl')
+  apply (simp add: star'_inv_step)
+  done
+
+(* Ex. 4.4 *)
+inductive iter :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" for r where
+  iter_refl: "iter r 0 x x"
+| iter_step: "r x y \<Longrightarrow> iter r n y z \<Longrightarrow> iter r (Suc n) x z"
+
+lemma "star r x y \<Longrightarrow> \<exists>n. iter r n x y"
+  apply (induction rule: star.induct)
+  apply (rule exI[where ?x = 0])
+  apply (rule iter_refl)
+  apply (erule exE)
+  apply (intro exI)
+  apply (rule exI[where ?x = "Suc n"])
+  apply (rule iter_step)
+
+(* The easy way: (this proofs the whole theorem after being given the induction)
+  apply (auto intro: iter_refl iter_step)
+  done
+*)
+
+thm exI
+
 end
